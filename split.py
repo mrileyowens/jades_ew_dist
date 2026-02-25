0b43ad420d89c4b8afccc73b03fd650a1a94729b
mrileyowens <m.riley.owens@gmail.com>
2026-02-25 10:48:24 -0800

    initial commit

---
+++ split.py
@@ -0,0 +1,35 @@
+# A file to separate the Endsley et al. (2024) photometric dropout catalog into two separate catalogs, based on which filter the break is in
+
+import os
+
+from astropy.io import fits
+from astropy.table import Table
+
+# Set common directories
+home = os.getcwd()
+data = f'{home}/data'
+
+# The file path to the combined catalog
+catalog = f'{data}/JADES_z6to9LBGcatalog_Endsley2024.fits'
+
+# Open the F775W and F090W dropout catalog from Endsley et al. (2024)
+with fits.open(catalog) as hdul:
+
+    # Format the catalog's data as a table
+    t = Table(hdul[1].data)
+
+    # For each dropout filter
+    for filter in ['F775W', 'F090W']:
+
+        # Make a mask for the dropout filter in the table
+        mask = t['dropoutType'] == filter
+
+        # Mask the table to just select the dropout galaxies in the given filter
+        t_new = t[mask]
+
+        # Create a new HDU from the masked table and replace the existing HDU
+        new_hdu = fits.BinTableHDU(data=t_new.as_array(), header=hdul[1].header)
+        hdul[1] = new_hdu
+
+        # Save the pared down catalog
+        hdul.writeto(f'{catalog.split('.')[0]}_{filter.lower()}_dropouts.fits', overwrite=True)
